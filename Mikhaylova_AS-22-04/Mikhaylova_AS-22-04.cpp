26a67c1402a2dc1fe25b40150d881bbc83a6cc50
Katyamikhaylova19 <megalogin32015@gmail.com>
2023-09-06 20:52:05 +0300

    Lab 0
    Сделала нулевую лабу

---
+++ Mikhaylova_AS-22-04/Mikhaylova_AS-22-04.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Enter a number: ";
+    int n;
+    std::cin >> n;
+    std::cout << "Your number: " << n;
+}
